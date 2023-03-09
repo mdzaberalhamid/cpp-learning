28854858755dc374326713d33e4f2a98d06cfe92
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-10 00:32:49 +0600

    Create 1_Cpp_Output.cpp

---
+++ 1_Cpp_Output.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // C++ output (printing text)
+  cout << "C++ output text 1";
+
+  // C++ output (printing text with new lines)
+  cout << "\n\n";
+  cout << "C++ output text 2\n";
+  cout << "C++ output text 3\n\n";
+
+  // Using endl for new lines
+  cout << "C++ output text 4" << endl;
+  cout << "Hope you understood!" << endl;
+
+  // Creating horizontal tab
+  cout << "\nHi,\t how are you?" << endl;
+
+  // Using backslash character
+  cout << "Hey\\I am learning C++" << endl;
+
+  // Using double quotation
+  cout << "You can also learn \"C++\" Programming!";
+
+  return 0;
+}
